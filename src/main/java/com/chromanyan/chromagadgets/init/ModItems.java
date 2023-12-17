dca163c2b6f2ae242ccbe0b885d9c2166d1eba7f
ChromaNyan <nyancatz593@gmail.com>
2023-12-17 12:40:13 -0600

    add white flag (still needs texture and recipe)

--- src/main/java/com/chromanyan/chromagadgets/init/ModItems.java
+++ src/main/java/com/chromanyan/chromagadgets/init/ModItems.java
@@ -2,6 +2,7 @@ package com.chromanyan.chromagadgets.init;
 
 import com.chromanyan.chromagadgets.ChromaGadgets;
 import com.chromanyan.chromagadgets.items.ItemSculkometer;
+import com.chromanyan.chromagadgets.items.ItemWhiteFlag;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,4 +13,5 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ChromaGadgets.MODID);
 
     public static final RegistryObject<Item> SCULKOMETER = ITEMS_REGISTRY.register("sculkometer", ItemSculkometer::new);
+    public static final RegistryObject<Item> WHITE_FLAG = ITEMS_REGISTRY.register("white_flag", ItemWhiteFlag::new);
 }
---
+++ src/main/java/com/chromanyan/chromagadgets/items/ItemWhiteFlag.java
@@ -0,0 +1,48 @@
+package com.chromanyan.chromagadgets.items;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.raid.Raid;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ItemWhiteFlag extends Item {
+    public ItemWhiteFlag() {
+        super(new Item.Properties()
+                .tab(CreativeModeTab.TAB_TOOLS)
+                .stacksTo(1));
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack itemStack, @Nullable Level level, List<Component> list, @NotNull TooltipFlag flag) {
+        list.add(Component.translatable("tooltip.chromagadgets.white_flag.1"));
+        list.add(Component.translatable("tooltip.chromagadgets.white_flag.2"));
+    }
+
+    @Override
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return InteractionResultHolder.pass(itemstack);
+        }
+
+        Raid raid = serverLevel.getRaidAt(player.blockPosition());
+        if (raid == null) {
+            return InteractionResultHolder.pass(itemstack);
+        }
+
+        raid.status = Raid.RaidStatus.LOSS; // i needed two access transformers for this
+
+        return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
+    }
+}
