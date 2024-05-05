e477435bbb65c51b20c2fbded7ac8bb1e510a7df
ChromaNyan <nyancatz593@gmail.com>
2024-05-05 17:53:03 -0500

    refactor: unused import

--- src/main/java/com/chromanyan/chromagadgets/items/ItemWhiteFlag.java
+++ src/main/java/com/chromanyan/chromagadgets/items/ItemWhiteFlag.java
@@ -7,7 +7,6 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.raid.Raid;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
