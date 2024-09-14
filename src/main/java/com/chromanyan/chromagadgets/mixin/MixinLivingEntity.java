60e9501448b04c8ac24e89d39122cb3798812f88
ChromaNyan <nyancatz593@gmail.com>
2024-09-13 23:18:12 -0500

    feat: make friction mixin less jank

--- src/main/java/com/chromanyan/chromagadgets/mixin/MixinLivingEntity.java
+++ src/main/java/com/chromanyan/chromagadgets/mixin/MixinLivingEntity.java
@@ -2,16 +2,12 @@ package com.chromanyan.chromagadgets.mixin;
 
 import com.chromanyan.chromagadgets.config.ModConfig;
 import com.chromanyan.chromagadgets.init.ModEnchantments;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = LivingEntity.class)
 public abstract class MixinLivingEntity {
@@ -19,24 +15,22 @@ public abstract class MixinLivingEntity {
     @Unique
     private static final ModConfig.Common chromaGadgets$config = ModConfig.COMMON;
 
-    // can't mixin inject into interfaces, otherwise i would have opted for that
-    @Redirect(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getFriction(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;)F"))
-    private float getFriction(BlockState instance, LevelReader levelReader, BlockPos blockPos, Entity entity) {
-        float originalReturn = instance.getFriction(levelReader, blockPos, entity);
-        if (!(entity instanceof LivingEntity livingEntity)) return originalReturn;
+    @ModifyExpressionValue(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getFriction(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/Entity;)F"))
+    private float getFriction(float original) {
+        LivingEntity trueThis = (LivingEntity)(Object) this;
 
         if (
-                originalReturn > chromaGadgets$config.defaultFriction.get().floatValue()
-                && EnchantmentHelper.getEnchantmentLevel(ModEnchantments.FRICTION.get(), livingEntity) > 0
+                original > chromaGadgets$config.defaultFriction.get().floatValue()
+                        && EnchantmentHelper.getEnchantmentLevel(ModEnchantments.FRICTION.get(), trueThis) > 0
         ) return chromaGadgets$config.defaultFriction.get().floatValue();
 
-        if (livingEntity.getBlockSpeedFactor() > 1.0F && chromaGadgets$config.ignoreSpeedyBlocks.get()) return originalReturn;
+        if (trueThis.getBlockSpeedFactor() > 1.0F && chromaGadgets$config.ignoreSpeedyBlocks.get()) return original;
 
         if (
-                EnchantmentHelper.getEnchantmentLevel(ModEnchantments.SLIPPERINESS.get(), livingEntity) > 0
-                && originalReturn < chromaGadgets$config.slipperyFriction.get().floatValue()
+                EnchantmentHelper.getEnchantmentLevel(ModEnchantments.SLIPPERINESS.get(), trueThis) > 0
+                        && original < chromaGadgets$config.slipperyFriction.get().floatValue()
         ) return chromaGadgets$config.slipperyFriction.get().floatValue();
 
-        return originalReturn;
+        return original;
     }
 }
